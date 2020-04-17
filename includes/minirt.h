5c1def3af20c42e7446ae457c61916bcf2c72199
pguthaus <pguthaus@student.42.fr>
2020-04-18 01:38:42 +0200

    Fixed header

--- includes/minirt.h
+++ includes/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: pguthaus <pguthaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 19:29:27 by pguthaus          #+#    #+#             */
-/*   Updated: 2020/04/17 18:33:37 by user42           ###   ########.fr       */
+/*   Updated: 2020/04/18 00:05:36 by pguthaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define MINIRT_H
 # include "world.h"
 # include "state.h"
-# include <mlx.h>
+# include "mlx.h"
 # include <float.h>
 # include <math.h>
 # define USAGE "Usage: ./minirt [-save] file"
--- srcs/output/window.c
+++ srcs/output/window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   window.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 03:12:31 by pguthaus          #+#    #+#             */
-/*   Updated: 2020/04/17 22:44:16 by user42           ###   ########.fr       */
+/*   Updated: 2020/04/18 01:37:49 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 #define POSITION_VEL 0.5
 #define ORIENTATION_VEL 0.1
 
-/*
 static void				move(int key, t_carry *c)
 {
 	t_camera			*cam;
 
-	cam = c->w->cameras[c->s.cam];
+	cam = c->w->cameras[c->s->cam];
 	if (key == KEY_W)
 		cam->position = ft_vec3f_add(cam->position, ft_vec3f_mul(cam->forward, POSITION_VEL));
 	else if (key == KEY_A)
@@ -34,14 +33,14 @@ static void				rotate(int key, t_carry *c)
 {
 	t_camera			*cam;
 
-	cam = c->w->cameras[c->s.cam];
-	if (key == KEY_PAD_2)
+	cam = c->w->cameras[c->s->cam];
+	if (key == KEY_ARROW_DOWN)
 		cam->forward = ft_vec3f_sub(cam->forward, ft_vec3f_init(0, ORIENTATION_VEL, 0));
-	if (key == KEY_PAD_8)
+	if (key == KEY_ARROW_UP)
 		cam->forward = ft_vec3f_add(cam->forward, ft_vec3f_init(0, ORIENTATION_VEL, 0));
-	if (key == KEY_PAD_4)
+	if (key == KEY_ARROW_LEFT)
 		cam->forward = ft_vec3f_sub(cam->forward, ft_vec3f_init(0, 0, ORIENTATION_VEL));
-	if (key == KEY_PAD_6)
+	if (key == KEY_ARROW_RIGHT)
 		cam->forward = ft_vec3f_add(cam->forward, ft_vec3f_init(0, 0, ORIENTATION_VEL));
 	cam->right = ft_vec3f_cross(cam->forward, ft_vec3f_init(0, 1, 0));
 	cam->up = ft_vec3f_cross(cam->forward, cam->right);
@@ -49,36 +48,35 @@ static void				rotate(int key, t_carry *c)
 
 static void				next_cam(t_carry *c)
 {
-	if (c->s.cam == c->w->cameras_count - 1)
-		c->s.cam = 0;
+	if (c->s->cam == c->w->cameras_count - 1)
+		c->s->cam = 0;
 	else
-		c->s.cam++;
+		c->s->cam++;
 }
 
-
-static void				on_key_pressed(int key, void *p_c)
+static int					on_key_pressed(int key, void *p_c)
 {
 	const t_carry		*c = (t_carry *)p_c;
 
 	ft_printf("Key pressed: %d\n", key);
 	if (key == KEY_ESC)
 	{
-		mlx_destroy_image(c->s.mlx_ptr, c->s.img_ptr);
-		mlx_destroy_window(c->s.mlx_ptr, c->s.win_ptr);
+		mlx_destroy_image(c->s->mlx_ptr, c->s->img_ptr);
+		mlx_destroy_window(c->s->mlx_ptr, c->s->win_ptr);
 		exit(0);
 	}
 	if (key == KEY_W || key == KEY_A || key == KEY_S || key == KEY_D)
 		move(key, (t_carry *)c);
-	else if (key == KEY_PAD_8 || key == KEY_PAD_6 || key == KEY_PAD_2 || key == KEY_PAD_4)
+	else if (key == KEY_ARROW_DOWN || key == KEY_ARROW_LEFT || key == KEY_ARROW_RIGHT || key == KEY_ARROW_UP)
 		rotate(key, (t_carry *)c);
 	else if (key == KEY_C)
 		next_cam((t_carry *)c);
 	else
-		return ;
+		return -1;
 	render((t_carry *)c);
 	do_output((t_carry *)c);
+	return (0);
 }
-*/
 
 void					init_window(t_carry *c)
 {
@@ -89,4 +87,5 @@ void					init_window(t_carry *c)
 	if (!(c->s->img_ptr = mlx_new_image(c->s->mlx_ptr, c->w->res[0], c->w->res[1])))
 		freexit(22, "Failed to mlx_new_image()", c);
 	c->s->image = mlx_get_data_addr(c->s->img_ptr, &(c->s->bits_per_pixel), &(c->s->size_line), &(c->s->endian));
+	mlx_key_hook(c->s->win_ptr, on_key_pressed, c);
 }
