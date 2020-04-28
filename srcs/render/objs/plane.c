8f23e4fe0870618adb092018a36c8aceecc97641
pguthaus <pguthaus@student.42.fr>
2020-04-28 21:18:49 +0200

    Plane intersections

--- srcs/render/objs/plane.c
+++ srcs/render/objs/plane.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 02:31:52 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/12/17 08:02:10 by pguthaus         ###   ########.fr       */
+/*   Updated: 2020/04/28 20:30:31 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@
 */
 t_bool				plane_intersects(t_plane plane, t_vec3f origin, t_vec3f dir, float *dist)
 {
-	const float		t = ft_vec3f_dot(ft_vec3f_sub(plane.position, origin), plane.orientation);
-	const float		denom = ft_vec3f_dot(plane.orientation, dir);
+	float denom;
+	float calcul_a;
+	float b;
 
-	if (denom < 1e-8)
-		return (false);
-	*dist = t / denom;
-	return (t >= 0);
+	denom = -(ft_vec3f_dot(plane.orientation, plane.position));
+	calcul_a = ft_vec3f_dot(origin, plane.orientation) + denom;
+	b = ft_vec3f_dot(dir, plane.orientation);
+	*dist = -(calcul_a / b);
+	return (*dist >= 0);
 }
 
 t_second_ray		plane_second_ray(t_plane plane, t_vec3f origin, t_vec3f dir, float dist)
